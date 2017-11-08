a68db4b1a343c54f03f606808ce674fc9ba4608f
anitahaurie <anitahaurie@gmail.com>
2017-11-08 18:58:05 -0500

    First pass candidate generation, incomplete

---
+++ TestCandidateGeneration/TestCandidateGeneration/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCandidateGeneration
+{
+    class Node
+    {
+        public Nucleotide mNucleotide;
+        public List<Node> mParents;
+        public List<Node> mChildren;
+        public uint mNeighbourIndex;
+        public int mDepth;
+        public Node()
+        {
+            mDepth = int.MaxValue;
+            mParents = new List<Node>();
+            mChildren = new List<Node>();
+        }
+
+        public Node(Nucleotide nucleotide, int depth, uint neighbourIdx = uint.MaxValue)
+        {
+            mNucleotide = nucleotide;
+            mParents = new List<Node>();
+            mChildren = new List<Node>();
+            mDepth = depth;
+            mNeighbourIndex = neighbourIdx;
+        }
+
+        public void SetChildren(List<Node> children)
+        {
+            mChildren = children;
+        }
+        public void SetParents(List<Node> parents)
+        {
+            mParents = parents;
+        }
+
+        public void SetNeighbourIndex(uint idx)
+        {
+            mNeighbourIndex = idx;
+        }
+    }
+}
---
+++ TestCandidateGeneration/TestCandidateGeneration/Ribozyme.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCandidateGeneration
+{
+    class Ribozyme
+    {
+        public String mSequence;
+        public String mStructure;
+        public String mCutSite;
+        public List<int> mRNALinkIndices;
+
+        public Ribozyme()
+        {
+            mRNALinkIndices = new List<int>();
+        }
+
+        public Ribozyme(String seq, String struc, String cutSite)
+        {
+            mSequence = seq;
+            mStructure = struc;
+            mCutSite = cutSite;
+            mRNALinkIndices = new List<int>();
+        }
+    }
+}
