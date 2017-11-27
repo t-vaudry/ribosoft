c94c57a3abcff17bd8b6e0d8822584366ebbe763
DTA <danhasgit@outlook.com>
2017-11-26 21:01:50 -0500

    Added annealing temp algo

--- RibosoftAlgo/src/functions.h
+++ RibosoftAlgo/src/functions.h
@@ -4,3 +4,4 @@
 
 extern "C" DLL_PUBLIC int math_add(int a, int b);
 extern "C" DLL_PUBLIC char* fold(const char* seq);
+extern "C" DLL_PUBLIC DLL_PUBLIC float anneal(const char* sequence, const char* structure, float na_concentration);
