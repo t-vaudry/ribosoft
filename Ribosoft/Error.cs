30a577c1e7e1a510a5c969efe7d7c7fe76f66a7a
t-vaudry <tvaudryread@gmail.com>
2017-12-06 15:08:37 -0500

    Namespacing the algos Minor changes to error codes

--- Ribosoft/Error.cs
+++ Ribosoft/Error.cs
@@ -9,13 +9,22 @@ namespace Ribosoft
     /* APPLICATION ERROR  : Range (-1)-(-999)     */
     /* USER ERROR         : Range (-1000)-(-1999) */
     /* SYSTEM ERROR       : Range (-2000)-(-2999) */
-    public enum R_ERROR : int
+    public enum R_STATUS : int
     {
-        RIBOSOFT_OK                 =  0,
-        R_INVALID_PARAMETER         = -1,
-        R_INVALID_NUCLEOTIDE        = -2,
-        R_INVALID_STRUCT_ELEMENT    = -3,
-        R_EMPTY_PARAMETER           = -4,
-        R_BAD_PAIR_MATCH            = -5,
+        R_OK                        =     0,
+        R_SUCCESS_LAST              =  1000,
+
+        R_INVALID_PARAMETER         =    -1,
+        R_INVALID_NUCLEOTIDE        =    -2,
+        R_INVALID_STRUCT_ELEMENT    =    -3,
+        R_EMPTY_PARAMETER           =    -4,
+        R_BAD_PAIR_MATCH            =    -5,
+        R_APPLICATION_ERROR_LAST    =  -999,
+
+        R_USER_ERROR_FIRST          = -1000,
+        R_USER_ERROR_LAST           = -1999,
+
+        R_SYSTEM_ERROR_FIRST        = -2000,
+        R_SYSTEM_ERROR_LAST         = -2999,
     };
 }
--- RibosoftAlgo/src/functions.h
+++ RibosoftAlgo/src/functions.h
@@ -3,10 +3,14 @@
 #include "dll.h"
 #include "error.h"
 
-typedef uint8_t idx_t;
+namespace ribosoft {
 
-extern "C" DLL_PUBLIC int math_add(int a, int b);
-extern "C" DLL_PUBLIC char* fold(const char* seq);
+    typedef uint8_t idx_t;
 
-extern "C" DLL_PUBLIC R_ERROR validate_sequence(const char* sequence);
-extern "C" DLL_PUBLIC R_ERROR validate_structure(const char* structure);
+    extern "C" DLL_PUBLIC int math_add(int a, int b);
+    extern "C" DLL_PUBLIC char* fold(const char* seq);
+
+    extern "C" DLL_PUBLIC R_STATUS validate_sequence(const char* sequence);
+    extern "C" DLL_PUBLIC R_STATUS validate_structure(const char* structure);
+
+}
