ee8dd461b41b6ebab7cb8ff50aeceeda936f5282
Chris Paslawski <chris@paslawski.me>
2017-12-09 02:47:20 -0500

    Deploy master to prod server

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,16 +4,20 @@
 set -e
 
 DEPLOY_URL="$1"
+DEPLOY_BRANCH="$2"
+
+# decrypt necessary files
+openssl aes-256-cbc -K $encrypted_201caca5971a_key -iv $encrypted_201caca5971a_iv -in .travis/secrets.tar.enc -out .travis/secrets.tar -d
+tar xf .travis/secrets.tar
 
 # prepare ssh environment
-echo -e "$SSH_CONFIG" >> ~/.ssh/config
+cat .travis/ssh_config >> ~/.ssh/config
 eval "$(ssh-agent -s)"
 
-# decrypt deployment key
-openssl aes-256-cbc -K $encrypted_201caca5971a_key -iv $encrypted_201caca5971a_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
+# add deployment key
 chmod 600 .travis/deploy_key
 ssh-add .travis/deploy_key
 
 # deploy to remote git repo
 git remote add deploy $DEPLOY_URL
-git push --force --quiet deploy develop
+git push --force --quiet deploy $DEPLOY_BRANCH
