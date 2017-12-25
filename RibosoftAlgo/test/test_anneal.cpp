8d3e5012fc13199cb8ee32df3da12acb2c56b717
daniel-adams <danhasgit@outlook.com>
2017-12-25 03:40:01 -0500

    Now using error codes [skip ci]

--- RibosoftAlgo/test/test_anneal.cpp
+++ RibosoftAlgo/test/test_anneal.cpp
@@ -22,6 +22,30 @@ TEST_CASE("simple sequence and structure", "[anneal]") {
 	REQUIRE(fabs(temp-73.9429) < DELTA);
 }
 
+TEST_CASE("invalid (0) concentration", "[anneal]") {
+	const char* sequence = "AAUUUCCCCGGGGG";
+	const char* structure = "0123abxyzABXYZ";
+	float na_concentration = 0.0;
+	float temp = anneal(sequence, structure, na_concentration);
+	REQUIRE(temp == R_APPLICATION_ERROR::R_INVALID_CONCENTRATION);
+}
+
+TEST_CASE("invalid base", "[anneal]") {
+	const char* sequence = "AAU_UCCCCGGGGG";
+	const char* structure = "0123abxyzABXYZ";
+	float na_concentration = 0.0;
+	float temp = anneal(sequence, structure, na_concentration);
+	REQUIRE(temp == R_APPLICATION_ERROR::R_INVALID_BASE);
+}
+
+TEST_CASE("mismatch between sequence and structure lengths", "[anneal]") {
+	const char* sequence = "AAUUCCCCGG";
+	const char* structure = "0123abxyzABXYZ";
+	float na_concentration = 0.0;
+	float temp = anneal(sequence, structure, na_concentration);
+	REQUIRE(temp == R_APPLICATION_ERROR::R_SEQUENCE_STRUCTURE_MISMATCH);
+}
+
 TEST_CASE("structure and sequence including all ignored characters", "[anneal]") {
 	const char* sequence = "AUCG ;:`[]~!@#$%^&*(){}/=\?+|-_AAAAAAAAAAAAAAAUUUUUUUUUUUUUUUCCCCCCCCCCCCCCCGGGGGGGGGGGGGGGAU";
 	const char* structure = "azAZ ;:`[]~!@#$%^&*(){}/=\?+|-_0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
