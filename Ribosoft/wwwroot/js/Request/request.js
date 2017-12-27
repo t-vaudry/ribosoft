5b971fd84f6cc30dd952035697f384b7cf90a626
Steve Ferreira <sjferreira516@gmail.com>
2017-12-27 00:10:56 -0500

    Support FASTA file upload for sequence input

--- Ribosoft/wwwroot/js/Request/request.js
+++ Ribosoft/wwwroot/js/Request/request.js
@@ -58,8 +58,41 @@ var app = new Vue({
             panelTitle.classList.toggle("collapsed");
             panelBody.classList.toggle("collapse");
         },
-        submit: function() {
+        processFASTAfile: function() { 
+            var inputField = document.getElementById("inputSequence");
+            var file = document.getElementById("FASTAfileInput").files[0];
+            var reader = new FileReader();
 
+            reader.onload = function(e) {
+                var firstSequence = false;
+                var text = reader.result;
+                var lines = text.split('\n');
+                var output = "";
+
+                for (var i = 0; i < lines.length; i++) {
+                    var line = lines[i];
+
+                    // Check if line starts with > (start of a new sequence)
+                    if (line.match(/^>/) && !firstSequence) {
+                        firstSequence = true;
+                    }
+                    else if (line.match(/^>/) && firstSequence) {
+                        // Start of 2nd sequence, stop processing
+                        break;
+                    }
+                    // Ignore blank lines
+                    else if (!line.match(/^\n/)) {
+                        output += line;
+                    }
+                }
+
+                inputField.value = output;
+            }
+
+            reader.readAsText(file)
+        },
+        submit: function() {
+            // TODO: Main vailidation driver
         }
     }
 });
