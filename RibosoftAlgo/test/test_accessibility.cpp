8aada62cc9f262a15fea14b03f2c5a8e93a31639
t-vaudry <tvaudryread@gmail.com>
2017-12-29 18:48:56 -0500

    Fixing warnings

--- RibosoftAlgo/test/test_accessibility.cpp
+++ RibosoftAlgo/test/test_accessibility.cpp
@@ -8,44 +8,44 @@
 using namespace ribosoft;
 
 TEST_CASE("Successful accessibility", "[accessibility]") {
-    float delta = NULL;
+    float delta = -1.0f;
     R_STATUS status = accessibility("CAACUGCAUGUGAUGUGUAUGUGCUAAUCGGAUGCAUGUAGCUAGUGCGAUGCAACUGCAUGUGAUGUGUAUGUGCUAAUCGGAUGCAUGUAGCUAGUGCGUGCGGCGCGUAAUGCUAGUCGUAGUCGUAGUGCUAGUGUGCUGCUAGCUGUAGUGCUAUCGAUCGAUGCUAGCUGUAGUCGAUGCAACUGCAUGUGAUGUGUAUGUGCUAAUCGGAUGCAUGUAGCUAGUGCGUGCGGCGCGUAAUGCUAGUCGUAGUCGUAGUGCUAGUGUGCUGCUAGCUGUAGUGCUAUCGAUCGAUGCUAGCUGUAGUCGAAUGCGGCGCGUAAUGCUAGUCGUAGUCGUAGUGCUAGUGUGCUGCUAGCUGUAGUGCUAUCGAUCGAUGCUAGCUGUAGUCGA", "fed..234", 200, 4, delta);
     REQUIRE(status == R_SUCCESS::R_STATUS_OK);
     REQUIRE(fabs(delta - 5.9f) < DELTA);
 }
 
 TEST_CASE("Invalid substrate sequence", "[accessibility]") {
-    float delta = NULL;
+    float delta = -1.0f;
     R_STATUS status = accessibility("cjwdjvbq", "fed..234", 200, 4, delta);
     REQUIRE(status == R_APPLICATION_ERROR::R_INVALID_NUCLEOTIDE);
-    REQUIRE(delta == NULL);
+    REQUIRE(delta == -1.0f);
 }
 
 TEST_CASE("Invalid cutsiteIndex", "[accessibility]") {
-    float delta = NULL;
+    float delta = -1.0f;
     R_STATUS status = accessibility("AUGC", "fed..234", -5, 4, delta);
     REQUIRE(status == R_APPLICATION_ERROR::R_OUT_OF_RANGE);
-    REQUIRE(delta == NULL);
+    REQUIRE(delta == -1.0f);
 
     status = accessibility("AUGC", "fed..234", 800, 4, delta);
     REQUIRE(status == R_APPLICATION_ERROR::R_OUT_OF_RANGE);
-    REQUIRE(delta == NULL);
+    REQUIRE(delta == -1.0f);
 }
 
 TEST_CASE("Invalid cutsite", "[accessibility]") {
-    float delta = NULL;
+    float delta = -1.0f;
     R_STATUS status = accessibility("AUGC", "fed..234", 200, -4, delta);
     REQUIRE(status == R_APPLICATION_ERROR::R_OUT_OF_RANGE);
-    REQUIRE(delta == NULL);
+    REQUIRE(delta == -1.0f);
 
     status = accessibility("AUGC", "fed..234", 200, 20, delta);
     REQUIRE(status == R_APPLICATION_ERROR::R_OUT_OF_RANGE);
-    REQUIRE(delta == NULL);
+    REQUIRE(delta == -1.0f);
 }
 
 TEST_CASE("Invalid template", "[accessibility]") {
-    float delta = NULL;
+    float delta = -1.0f;
     R_STATUS status = accessibility("AUGC", "fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234fed..234", 2, 2, delta);
     REQUIRE(status == R_APPLICATION_ERROR::R_INVALID_TEMPLATE_LENGTH);
-    REQUIRE(delta == NULL);
+    REQUIRE(delta == -1.0f);
 }
