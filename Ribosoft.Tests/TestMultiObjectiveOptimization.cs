c15045bae677ef5c695c7ef2e0b06e2a70fdd21f
t-vaudry <tvaudryread@gmail.com>
2017-12-30 15:43:54 -0500

    Added exceptions and comments More unit tests cover multiple dimensions, and exception case

--- Ribosoft.Tests/TestMultiObjectiveOptimization.cs
+++ Ribosoft.Tests/TestMultiObjectiveOptimization.cs
@@ -7,7 +7,53 @@ namespace Ribosoft.Tests
     public class TestMultiObjectiveOptimization
     {
         [Fact]
-        public void ValidInput()
+        public void EqualInput()
+        {
+            MultiObjectiveOptimization.MultiObjectiveOptimizer multiObjectiveOptimizer = new MultiObjectiveOptimization.MultiObjectiveOptimizer();
+
+            float[] first = { 1.0f, 1.0f };
+            float[] second = { 1.0f, 1.0f };
+
+            MultiObjectiveOptimization.Candidate one = new MultiObjectiveOptimization.Candidate(first);
+            MultiObjectiveOptimization.Candidate two = new MultiObjectiveOptimization.Candidate(second);
+
+            List<MultiObjectiveOptimization.Candidate> candidates = new List<MultiObjectiveOptimization.Candidate>();
+            candidates.Add(one);
+            candidates.Add(two);
+
+            multiObjectiveOptimizer.Candidates = candidates;
+
+            multiObjectiveOptimizer.Optimize(1);
+
+            Assert.Equal(1, one.Rank);
+            Assert.Equal(1, two.Rank);
+        }
+
+        [Fact]
+        public void NearlyEqualInput()
+        {
+            MultiObjectiveOptimization.MultiObjectiveOptimizer multiObjectiveOptimizer = new MultiObjectiveOptimization.MultiObjectiveOptimizer();
+
+            float[] first = { 1.0f, 1.0f };
+            float[] second = { 1.05f, 1.0f };
+
+            MultiObjectiveOptimization.Candidate one = new MultiObjectiveOptimization.Candidate(first);
+            MultiObjectiveOptimization.Candidate two = new MultiObjectiveOptimization.Candidate(second);
+
+            List<MultiObjectiveOptimization.Candidate> candidates = new List<MultiObjectiveOptimization.Candidate>();
+            candidates.Add(one);
+            candidates.Add(two);
+
+            multiObjectiveOptimizer.Candidates = candidates;
+
+            multiObjectiveOptimizer.Optimize(1);
+
+            Assert.Equal(1, one.Rank);
+            Assert.Equal(1, two.Rank);
+        }
+
+        [Fact]
+        public void Valid2DInput()
         {
             MultiObjectiveOptimization.MultiObjectiveOptimizer multiObjectiveOptimizer = new MultiObjectiveOptimization.MultiObjectiveOptimizer();
 
@@ -30,13 +76,59 @@ namespace Ribosoft.Tests
         }
 
         [Fact]
-        public void AnotherValidInput()
+        public void Valid3DInput()
+        {
+            MultiObjectiveOptimization.MultiObjectiveOptimizer multiObjectiveOptimizer = new MultiObjectiveOptimization.MultiObjectiveOptimizer();
+
+            float[] first = { 1.0f, 1.0f, 1.0f };
+            float[] second = { 2.0f, 2.0f, 2.0f };
+
+            MultiObjectiveOptimization.Candidate one = new MultiObjectiveOptimization.Candidate(first);
+            MultiObjectiveOptimization.Candidate two = new MultiObjectiveOptimization.Candidate(second);
+
+            List<MultiObjectiveOptimization.Candidate> candidates = new List<MultiObjectiveOptimization.Candidate>();
+            candidates.Add(one);
+            candidates.Add(two);
+
+            multiObjectiveOptimizer.Candidates = candidates;
+
+            multiObjectiveOptimizer.Optimize(1);
+
+            Assert.Equal(1, one.Rank);
+            Assert.Equal(2, two.Rank);
+        }
+
+        [Fact]
+        public void Valid4DInput()
+        {
+            MultiObjectiveOptimization.MultiObjectiveOptimizer multiObjectiveOptimizer = new MultiObjectiveOptimization.MultiObjectiveOptimizer();
+
+            float[] first = { 1.0f, 1.0f, 1.0f, 1.0f };
+            float[] second = { 2.0f, 2.0f, 2.0f, 2.0f };
+
+            MultiObjectiveOptimization.Candidate one = new MultiObjectiveOptimization.Candidate(first);
+            MultiObjectiveOptimization.Candidate two = new MultiObjectiveOptimization.Candidate(second);
+
+            List<MultiObjectiveOptimization.Candidate> candidates = new List<MultiObjectiveOptimization.Candidate>();
+            candidates.Add(one);
+            candidates.Add(two);
+
+            multiObjectiveOptimizer.Candidates = candidates;
+
+            multiObjectiveOptimizer.Optimize(1);
+
+            Assert.Equal(1, one.Rank);
+            Assert.Equal(2, two.Rank);
+        }
+
+        [Fact]
+        public void InvalidInput()
         {
             MultiObjectiveOptimization.MultiObjectiveOptimizer multiObjectiveOptimizer = new MultiObjectiveOptimization.MultiObjectiveOptimizer();
 
             float[] first = { 2.0f, 1.0f, 1.0f };
             float[] second = { 2.0f, 2.0f, 2.0f };
-            float[] third = { 3.0f, 2.0f, 3.0f };
+            float[] third = { 3.0f, 2.0f };
 
             MultiObjectiveOptimization.Candidate one = new MultiObjectiveOptimization.Candidate(first);
             MultiObjectiveOptimization.Candidate two = new MultiObjectiveOptimization.Candidate(second);
@@ -49,11 +141,11 @@ namespace Ribosoft.Tests
 
             multiObjectiveOptimizer.Candidates = candidates;
 
-            multiObjectiveOptimizer.Optimize(1);
-
-            Assert.Equal(1, one.Rank);
-            Assert.Equal(2, two.Rank);
-            Assert.Equal(3, three.Rank);
+            try {
+                multiObjectiveOptimizer.Optimize(1);
+            } catch (MultiObjectiveOptimization.MultiObjectiveOptimizationException e) {
+                Assert.Equal(e.Message, "Candidates have different number of fitness values!");
+            }
         }
     }
 }
--- Ribosoft/MultiObjectiveOptimization/MultiObjectiveOptimization.cs
+++ Ribosoft/MultiObjectiveOptimization/MultiObjectiveOptimization.cs
@@ -14,30 +14,46 @@ namespace Ribosoft.MultiObjectiveOptimization {
             Candidates = null;
         }
 
+        /* Optimization using Pareto Ranking
+        ** Recursive implementation where candidates are compared
+        ** to find dominated candidates. Those who are not dominated
+        ** are ranked, and removed from the list. This happens
+        ** recursively until there are no more candidates to rank.
+        */
         public void Optimize(int Rank)
         {
+            // List for the current rank
             List<Candidate> FrontCandidates = new List<Candidate>();
 
             foreach (Candidate Victim in Candidates) {
                 bool Dominated = false;
 
                 foreach (Candidate Dominator in Candidates) {
+                    // If the same candidate, continue
                     if (Dominator == Victim) {
                         continue;
                     }
 
-                    if (ParetoDominate(Victim, Dominator)) {
-                        Dominated = true;
-                        break;
+                    // Check for dominance, and break if candidate is dominated
+                    try {
+                        if (ParetoDominate(Victim, Dominator))
+                        {
+                            Dominated = true;
+                            break;
+                        }
+                    } catch (MultiObjectiveOptimizationException e) {
+                        throw e;
                     }
                 }
 
+                // If not dominated by any other candidate, add to Pareto Front
                 if (!Dominated) {
                     FrontCandidates.Add(Victim);
                 }
             }
 
-            // TODO: Remove, I don't think this is necessary anymore?
+            // Rank non-dominated candidates, and remove from list
+            // If all candidates are dominated, rank them all equally
             if (FrontCandidates.Count != 0) {
                 foreach (Candidate RankedCandidate in FrontCandidates) {
                     RankedCandidate.Rank = Rank;
@@ -51,16 +67,28 @@ namespace Ribosoft.MultiObjectiveOptimization {
                 Candidates.Clear();
             }
 
+            // Recursively call function to continue ranking
             if (Candidates.Count != 0) {
                 Rank++;
                 Optimize(Rank);
             }
         }
 
+        /* Pareto Dominance
+        ** A vector x dominates y:
+        **   1. If fi(x) <= fi(y) for all i functions of f, and
+        **   2. There is at least one i such that fi(x) < fi(y)
+        */
         private bool ParetoDominate(Candidate Victim, Candidate Dominator)
         {
             bool Dominated = true;
             bool StrictlyDominated = false;
+
+            // If two candidates have different number of fitness values, something went wrong
+            if (Victim.FitnessValues.Length != Dominator.FitnessValues.Length) {
+                throw new MultiObjectiveOptimizationException("Candidates have different number of fitness values!");
+            }
+
             var Properties = Victim.FitnessValues.Zip(Dominator.FitnessValues, (x, y) => new { Victim = x, Dominator = y });
             foreach (var Property in Properties) {
                 if (Property.Dominator <= Property.Victim) {
