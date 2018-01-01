0662c7af70dc7ebce16e33e57d982ea060aeee2c
t-vaudry <tvaudryread@gmail.com>
2018-01-01 18:52:25 -0500

    Implemented folding with suboptimal results Energy range is currently hard coded to 5 kcal/mol (this could be a user input)

--- RibosoftAlgo/src/functions.h
+++ RibosoftAlgo/src/functions.h
@@ -9,12 +9,17 @@ RIBOSOFT_NAMESPACE_START
 
 typedef uint8_t idx_t;
 
+struct fold_output {
+    char* structure;
+    float energy;
+};
+
 extern "C" DLL_PUBLIC int math_add(int a, int b);
-extern "C" DLL_PUBLIC char* fold(const char* seq);
 
 extern "C" DLL_PUBLIC R_STATUS validate_sequence(const char* sequence);
 extern "C" DLL_PUBLIC R_STATUS validate_structure(const char* structure);
 
+extern "C" DLL_PUBLIC R_STATUS fold(const char* sequence, /*out*/ fold_output*& output);
 extern "C" DLL_PUBLIC R_STATUS structure(const char* candidate, const char* ideal, /*out*/ float& distance);
 
 RIBOSOFT_NAMESPACE_END
