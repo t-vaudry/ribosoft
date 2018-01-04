fe1a2df6e08c59b82c9ad29ba8fa4ee6a99542d5
Chris Paslawski <chris@paslawski.me>
2018-01-04 04:51:28 -0500

    Add very basic email service

--- Ribosoft/Services/EmailSender.cs
+++ Ribosoft/Services/EmailSender.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace Ribosoft.Services
 {
@@ -9,9 +13,32 @@ namespace Ribosoft.Services
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
+        private readonly SendGridClient _sendGridClient;
+        private readonly string _senderEmail;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            if (configuration["SendGridAPIKey"] == null)
+            {
+                return;
+            }
+
+            _sendGridClient = new SendGridClient(configuration["SendGridAPIKey"]);
+            _senderEmail = configuration["SenderEmail"];
+        }
+
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            return Task.CompletedTask;
+            if (_sendGridClient == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var from = new EmailAddress(_senderEmail, "Ribosoft");
+            var to = new EmailAddress(email);
+            var plainTextContent = Regex.Replace(message, "<[^>]*>", "");
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, message);
+            return _sendGridClient.SendEmailAsync(msg);
         }
     }
 }
