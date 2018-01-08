e946d8b91c2c14450db8f8662d444a3e0fff24b9
t-vaudry <tvaudryread@gmail.com>
2018-01-08 15:11:28 -0500

    Add another unit test (valid)

--- RibosoftAlgo/test/test_fold.cpp
+++ RibosoftAlgo/test/test_fold.cpp
@@ -15,6 +15,22 @@ TEST_CASE("default", "[fold]") {
 
     REQUIRE(strcmp(output[35].structure, "((((..(.....).))))..") == 0);
     REQUIRE(output[35].energy == 3.00f);
+
+    REQUIRE(size == 51);
+}
+
+TEST_CASE("valid", "[fold]") {
+    fold_output* output = nullptr;
+    size_t size;
+    R_STATUS status = fold("AUUUUAGUGCUGAUGGCCAAUGCGCGAACCCAUCGGCGCUGUGA", output, size);
+    REQUIRE(status == R_SUCCESS::R_STATUS_OK);
+    REQUIRE(strcmp(output[1].structure, ".((.((((((((((((.............)))))))))))).))") == 0);
+    REQUIRE(output[1].energy == -16.20f);
+
+    REQUIRE(strcmp(output[17].structure, ".....((((((((((((........)...)))))))))))....") == 0);
+    REQUIRE(output[17].energy == -14.50f);
+
+    REQUIRE(size == 173);
 }
 
 TEST_CASE("invalid sequence", "[fold]") {
