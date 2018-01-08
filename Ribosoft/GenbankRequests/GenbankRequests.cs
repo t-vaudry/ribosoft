636fa13842d8021e6b870392f784676fe4ec5282
daniel-adams <danhasgit@outlook.com>
2018-01-07 21:43:02 -0500

    Finished Genbank Handler [skip ci]

--- Ribosoft/GenbankRequests/GenbankRequests.cs
+++ Ribosoft/GenbankRequests/GenbankRequests.cs
@@ -16,45 +16,46 @@ namespace Ribosoft.GenbankRequests
     {
 
         private static readonly HttpClient client = new HttpClient();
-        
+
         private static String eUtilsHostString = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi";
-        private static String genbankHostString= "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi";
+        private static String genbankHostString = "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi";
 
         static public string RunRequest(string accession)
         {
-            try
             {
-                var id = getID(accession);
-                var rawSequence = getSequence(id);
-                Console.WriteLine("Done.");
-                return rawSequence;
-            }
-            catch (System.Exception exc)
-            {
-                throw new GenbankRequestsException("An error occurred while running Genbank requests", exc);
+                try
+                {
+                    string id = getIDAsync(accession).Result;
+                    var rawSequence = getSequenceAsync(id).Result;
+                    return rawSequence;
+                }
+                catch (System.Exception exc)
+                {
+                    throw new GenbankRequestsException("An error occurred while running Genbank requests", exc);
+                }
             }
         }
 
-        static public string getID(String accession)
+        static public async Task<string> getIDAsync(String accession)
         {
             // example: https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nuccore&term=M73077
             var databaseString = "db=nuccore";
             var searchtermString = "term=" + accession;
             var requestURL = string.Concat(eUtilsHostString, "?", databaseString, "&", searchtermString);
 
-            var result = client.GetAsync(requestURL).Result;
-            if (!result.IsSuccessStatusCode)
-            {
-                throw new GenbankRequestsException(String.Format("EUtils request was not successful (Response Code {0})", result.StatusCode));    
-            }
-            string responseString = result.Content.ReadAsStringAsync().Result;
+            var result = await client.GetAsync(requestURL);
+            result.EnsureSuccessStatusCode();
+
+            var responseString = await result.Content.ReadAsStringAsync();
+            var xmlIndex = responseString.IndexOf("<eSearchResult>");
+            var xmlString = responseString.Substring(xmlIndex);
 
             XmlDocument doc = new XmlDocument();
             string id = "";
             try
             {
-                doc.LoadXml(responseString);
-                XmlNode node = doc.DocumentElement.SelectSingleNode("eSearchResult/IdList/Id");
+                doc.LoadXml(xmlString);
+                XmlNode node = doc.DocumentElement.SelectSingleNode("/eSearchResult/IdList/Id");
                 id = node.InnerText;
             }
             catch (System.Exception exc)
@@ -66,7 +67,7 @@ namespace Ribosoft.GenbankRequests
 
         }
 
-        static public string getSequence(String id)
+        static public async Task<string> getSequenceAsync(String id)
         {
             // example: https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=183617&db=nuccore&report=genbank&fmt_mask=0&maxdownloadsize=1000000&rettype=fasta&retmode=text
             string idString = "id=" + id;
@@ -78,12 +79,9 @@ namespace Ribosoft.GenbankRequests
             string retmodeString = "retmode=text";
             var requestURL = string.Concat(genbankHostString, "?", idString, "&", databaseString, "&", reportString, "&", fmtmaskString, "&", maxdownloadString, "&", rettypeString, "&", retmodeString);
 
-            var result = client.GetAsync(requestURL).Result;
-            if (!result.IsSuccessStatusCode)
-            {
-                throw new GenbankRequestsException(String.Format("Genbank request was not successful (Response Code {0})", result.StatusCode));
-            }
-            string responseString = result.Content.ReadAsStringAsync().Result;
+            var result = await client.GetAsync(requestURL);
+            result.EnsureSuccessStatusCode();
+            var responseString = await result.Content.ReadAsStringAsync();
 
             var sequenceIndex = responseString.IndexOf("\n") + 1;
             if (sequenceIndex == -1)
@@ -103,6 +101,5 @@ namespace Ribosoft.GenbankRequests
             return sequenceString;
 
         }
-
     }
 }
