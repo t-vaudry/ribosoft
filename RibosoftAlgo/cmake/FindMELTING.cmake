4e863841cca5f7d9578dd8d2f9f1e9459217f7ef
t-vaudry <tvaudryread@gmail.com>
2018-01-29 00:05:13 -0500

    Modifying library name and version

--- RibosoftAlgo/cmake/FindMELTING.cmake
+++ RibosoftAlgo/cmake/FindMELTING.cmake
@@ -2,10 +2,10 @@ find_library(MELTING_LIBRARY
         NAMES
         MELTING
         PATH_SUFFIXES
-        melting_v4/lib/)
+        melting/lib/)
 
 find_path(MELTING_INCLUDE_DIR
         NAMES
         melting.h
         PATH_SUFFIXES
-        melting_v4/include/)
+        melting/include/)
