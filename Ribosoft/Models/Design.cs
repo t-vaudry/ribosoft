0fd73b48017c642917d2950d3114228c569adec9
Chris Paslawski <chris@paslawski.me>
2018-02-03 20:25:47 -0500

    Use IRankable for MOO and batching

--- Ribosoft/Models/Design.cs
+++ Ribosoft/Models/Design.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Ribosoft.MultiObjectiveOptimization;
 
 namespace Ribosoft.Models
 {
-    public class Design : BaseEntity
+    public class Design : BaseEntity, IRankable<float>
     {
         public int Id { get; set; }
         public int JobId { get; set; }
         public string Sequence { get; set; }
         public int Rank { get; set; }
+
         [Display(Name = "Temperature Score")]
         public float? TemperatureScore { get; set; }
         [Display(Name = "Specificity Score")]
@@ -22,5 +24,13 @@ namespace Ribosoft.Models
         public float? StructureScore { get; set; }
 
         public virtual Job Job { get; set; }
+
+        public IEnumerable<float> Comparables => new []
+        {
+            TemperatureScore.GetValueOrDefault(),
+            SpecificityScore.GetValueOrDefault(),
+            AccessibilityScore.GetValueOrDefault(),
+            StructureScore.GetValueOrDefault()
+        };
     }
 }
