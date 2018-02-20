d6bcd08943fa2e0d923b4d40e3dbc03979cd2408
Steve Ferreira <sjferreira516@gmail.com>
2018-02-20 04:55:33 -0500

    Add UI elements and js functions to perform filtering operations

---
+++ Ribosoft/wwwroot/js/Jobs/details.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var params = (new URL(document.location)).searchParams;
+
+    if (params.has("filterParam")) {
+        document.getElementById("filterOptions").classList.toggle('hide');
+        document.getElementById("filterValue").value = params.get("filterValue");
+    }
+    else {
+        document.getElementById("showFilterOptions").classList.toggle('hide');
+    }
+});
+
+document.getElementById("filterSubmit").addEventListener("click", function() {
+    var params = (new URL(document.location)).searchParams;
+    var filterParam = document.getElementById("filterParam");
+    var filterCondition = document.getElementById("filterCondition");
+    var filterValue = document.getElementById("filterValue");
+    var url = document.URL;
+
+    if (params.has("sortOrder")) {
+        var sortOrder = params.get('sortOrder');
+        url = url.substring(0, url.indexOf('?'));
+        url += "?sortOrder="+sortOrder+"&";
+    }
+    else {
+        url = url.substring(0, url.indexOf('?'));
+        url += "?";
+    }
+    url += "filterParam="+filterParam.value;
+    url += "&filterCondition="+filterCondition.value;
+    url += "&filterValue="+filterValue.value;
+
+    window.location.href = url;
+});
+
+document.getElementById("refineResults").addEventListener("click", function() {
+    var refineResults = document.getElementById("showFilterOptions");
+    var filterOptions = document.getElementById("filterOptions");
+
+    refineResults.classList.toggle('hide');
+    filterOptions.classList.toggle('hide');
+});
