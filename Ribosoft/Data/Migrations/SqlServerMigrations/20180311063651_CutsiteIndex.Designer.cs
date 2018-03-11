7bfed40e39b05db1cbd08aaa8a1aefa2d220186a
Chris Paslawski <chris@paslawski.me>
2018-03-11 01:38:29 -0500

    Missing SqlServer migration

--- Ribosoft/Data/Migrations/SqlServerMigrations/20180311063651_CutsiteIndex.Designer.cs
+++ Ribosoft/Data/Migrations/SqlServerMigrations/20180311063651_CutsiteIndex.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Ribosoft.Data.Migrations.SqlServerMigrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180311063651_CutsiteIndex")]
+    partial class CutsiteIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
