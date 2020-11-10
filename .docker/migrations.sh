b008a4c728633c2af4e70690700c8fb284de8c9b
t-vaudry <tvaudryread@gmail.com>
2020-11-09 21:37:27 -0500

    Update docker image

--- .docker/migrations.sh
+++ .docker/migrations.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 dotnet tool install --global dotnet-ef
 export PATH="$PATH:$HOME/.dotnet/tools/"
-run_cmd="dotnet run --urls http://*:80"
+run_cmd="dotnet run --configuration Release --urls http://*:80"
 # test the db connection
 dotnet restore
-until dotnet-ef database update -c NpgsqlDbContext; do
+until dotnet-ef database update -c NpgsqlDbContext --configuration Release; do
 	>&2 echo "DB is starting up"
 	sleep 1
 done
