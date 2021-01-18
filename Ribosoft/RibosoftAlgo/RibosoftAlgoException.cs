01f5d820f4b90e2fd8ae72c004bbd7482286bb9e
t-vaudry <tvaudryread@gmail.com>
2021-01-18 17:10:15 -0500

    codefactor: fixing maintainability issues

---
+++ Ribosoft/RibosoftAlgo/RibosoftAlgoException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ribosoft.RibosoftAlgo
+{
+    public class RibosoftAlgoException : Exception
+    {
+        public R_STATUS Code { get; set; }
+
+        public RibosoftAlgoException(R_STATUS code)
+        {
+            this.Code = code;
+        }
+
+        public RibosoftAlgoException()
+            : this(R_STATUS.R_APPLICATION_ERROR_LAST)
+        {
+        }
+    }
+}
