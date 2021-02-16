5496528d17f56ce33c1ebf88d7787718cdd5d356
t-vaudry <tvaudryread@gmail.com>
2021-02-15 22:06:25 -0500

    New filter logic and sorting logic
    Resolves #86

--- Ribosoft/ClientApp/boot.js
+++ Ribosoft/ClientApp/boot.js
@@ -1,9 +1,13 @@
 import './css/site.css';
 import 'bootstrap';
 
+require('jquery-ui-themes/themes/redmond/jquery-ui.css');
+require('jquery-ui-themes/themes/redmond/theme.css');
+require('structured-filter/css/structured-filter.css');
+
 window.QRious = require('qrious');
 
 import fontawesome from '@fortawesome/fontawesome';
-import { faEdit, faPlusCircle, faInfoCircle, faTrash, faArrowLeft, faCheck, faExclamationTriangle, faCircleNotch, faQuestionCircle, faFilter, faSortUp, faSortDown, faDownload } from '@fortawesome/fontawesome-free-solid'
+import { faEdit, faPlusCircle, faInfoCircle, faTrash, faArrowLeft, faCheck, faExclamationTriangle, faCircleNotch, faQuestionCircle, faFilter, faSortUp, faSortDown, faSort, faDownload } from '@fortawesome/fontawesome-free-solid'
 
-fontawesome.library.add(faEdit, faPlusCircle, faInfoCircle, faTrash, faArrowLeft, faCheck, faExclamationTriangle, faCircleNotch, faQuestionCircle, faFilter, faSortUp, faSortDown, faDownload);
+fontawesome.library.add(faEdit, faPlusCircle, faInfoCircle, faTrash, faArrowLeft, faCheck, faExclamationTriangle, faCircleNotch, faQuestionCircle, faFilter, faSortUp, faSortDown, faSort, faDownload);
--- Ribosoft/Models/JobsViewModels/JobDetailsViewModel.cs
+++ Ribosoft/Models/JobsViewModels/JobDetailsViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using cloudscribe.Pagination.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
 
 namespace Ribosoft.Models.JobsViewModels
 {
@@ -10,6 +11,74 @@ namespace Ribosoft.Models.JobsViewModels
      */
     public class JobDetailsViewModel
     {
+        /*! \struct Filter
+         * \brief Filter structure to define a filter
+         */
+        public struct Filter
+        {
+            /*! \property param
+             * \brief Filter paramerter
+             */
+            public string param;
+
+            /*! \property condition
+             * \brief Filter condition
+             */
+            public string condition;
+
+            /*! \property value
+             * \brief Filter value
+             */
+            public string value;
+
+            /*! \fn GetJson
+             * \brief Helper function to generate the JSON string for filtering
+             * \return JSON string of filter data
+             */
+            public string GetJson()
+            {
+                JObject json = new JObject();
+                json["field"] = new JObject(new JProperty("label", GetLabel(param)), new JProperty("value", param));
+                json["operator"] = new JObject(new JProperty("label", GetLabel(condition)), new JProperty("value", condition));
+                json["value"] = new JObject(new JProperty("label", value), new JProperty("value", value));
+                return json.ToString();
+            }
+
+            /*! \fn GetLabel
+             * \brief Get label string from parameter id
+             * \param param Parameter ID
+             * \return Label str
+             */
+            private string GetLabel(string param)
+            {
+                switch(param)
+                {
+                    case "Rank":
+                        return "Rank";
+                    case "HighestTemperatureScore":
+                        return "Highest Temperature Score";
+                    case "DesiredTemperatureScore":
+                        return "Desired Temperature Score";
+                    case "AccessibilityScore":
+                        return "Accessibility Score";
+                    case "SpecificityScore":
+                        return "Specificity Score";
+                    case "StructureScore":
+                        return "Structure Score";
+                    case "eq":
+                        return "=";
+                    case "ne":
+                        return "!=";
+                    case "gt":
+                        return ">";
+                    case "lt":
+                        return "<";
+                    default:
+                        return "";
+                }
+            }
+        }
+
         /*! \property Job
          * \brief Current job
          */
@@ -25,30 +94,10 @@ namespace Ribosoft.Models.JobsViewModels
          */
         public string SortOrder { get; set; } = "";
 
-        /*! \property FilterParams
-         * \brief List of available filter parameters
+        /*! \property FilterList
+         * \brief List of filter elements
          */
-        public IEnumerable<SelectListItem> FilterParams { get; set; }
-
-        /*! \property FilterConditions
-         * \brief List of available filter conditions
-         */
-        public IEnumerable<SelectListItem> FilterConditions { get; set; }
-
-        /*! \property FilterParam
-         * \brief Current value of filter parameter
-         */
-        public string FilterParam { get; set; }
-
-        /*! \property FilterCondition
-         * \brief Current value of filter condition
-         */
-        public string FilterCondition { get; set; }
-
-        /*! \property FilterValue
-         * \brief Current value of filter value
-         */
-        public string FilterValue { get; set; }
+        public List<Filter> FilterList { get; set; }
 
         /*! \fn JobDetailsViewModel
          * \brief Default constructor
@@ -56,6 +105,7 @@ namespace Ribosoft.Models.JobsViewModels
         public JobDetailsViewModel()
         {
             Designs = new PagedResult<Design>();
+            FilterList = new List<Filter>();
         }
     }
 }
