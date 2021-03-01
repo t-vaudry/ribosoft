b011fd50422445cabb19b924d22ae4c877cc7336
Ankit Rajan <ankitrajan@hotmail.com>
2021-03-01 15:22:14 -0500

    Update tests

--- Ribosoft.Tests/TestMultiObjectiveOptimization.cs
+++ Ribosoft.Tests/TestMultiObjectiveOptimization.cs
@@ -57,17 +57,17 @@ namespace Ribosoft.Tests
         }
 
         [Fact]
-        public void NearlyEqualInput()
+        public void PartialRanking()
         {
             MultiObjectiveOptimization.MultiObjectiveOptimizer multiObjectiveOptimizer = new MultiObjectiveOptimization.MultiObjectiveOptimizer();
 
             Design one = new Design
             {
-                AccessibilityScore = 1.0f,
-                HighestTemperatureScore = 1.0f,
-                DesiredTemperatureScore = 1.0f,
-                SpecificityScore = 1.0f,
-                StructureScore = 1.0f,
+                AccessibilityScore = 3.0f,
+                HighestTemperatureScore = 10.0f,
+                DesiredTemperatureScore = 2.0f,
+                SpecificityScore = 2.0f,
+                StructureScore = 3.0f,
                 Job = new Job
                 {
                     DesiredTempTolerance = 0.05f,
@@ -80,11 +80,62 @@ namespace Ribosoft.Tests
 
             Design two = new Design
             {
+                AccessibilityScore = 2.0f,
+                HighestTemperatureScore = 9.0f,
+                DesiredTemperatureScore = 3.0f,
+                SpecificityScore = 2.0f,
+                StructureScore = 2.0f,
+                Job = new Job
+                {
+                    DesiredTempTolerance = 0.05f,
+                    HighestTempTolerance = 0.05f,
+                    SpecificityTolerance = 0.05f,
+                    AccessibilityTolerance = 0.05f,
+                    StructureTolerance = 0.05f
+                }
+            };
+
+            Design three = new Design
+            {
                 AccessibilityScore = 1.0f,
-                HighestTemperatureScore = 1.05f,
-                DesiredTemperatureScore = 1.0f,
-                SpecificityScore = 1.0f,
+                HighestTemperatureScore = 8.0f,
+                DesiredTemperatureScore = 4.0f,
+                SpecificityScore = 3.0f,
                 StructureScore = 1.0f,
+                Job = new Job
+                {
+                    DesiredTempTolerance = 0.05f,
+                    HighestTempTolerance = 0.05f,
+                    SpecificityTolerance = 0.05f,
+                    AccessibilityTolerance = 0.05f,
+                    StructureTolerance = 0.05f
+                }
+            };
+
+            Design four = new Design
+            {
+                AccessibilityScore = 1.0f,
+                HighestTemperatureScore = 8.0f,
+                DesiredTemperatureScore = 4.0f,
+                SpecificityScore = 3.0f,
+                StructureScore = 1.0f,
+                Job = new Job
+                {
+                    DesiredTempTolerance = 0.05f,
+                    HighestTempTolerance = 0.05f,
+                    SpecificityTolerance = 0.05f,
+                    AccessibilityTolerance = 0.05f,
+                    StructureTolerance = 0.05f
+                }
+            };
+
+            Design five = new Design
+            {
+                AccessibilityScore = 7.0f,
+                HighestTemperatureScore = 4.0f,
+                DesiredTemperatureScore = 7.0f,
+                SpecificityScore = 1.0f,
+                StructureScore = 7.0f,
                 Job = new Job
                 {
                     DesiredTempTolerance = 0.05f,
@@ -98,13 +149,89 @@ namespace Ribosoft.Tests
             List<Design> designs = new List<Design>
             {
                 one,
-                two
+                two,
+                three,
+                four,
+                five
             };
 
             multiObjectiveOptimizer.Optimize(designs, 1);
 
             Assert.Equal(1, one.Rank);
             Assert.Equal(1, two.Rank);
+            Assert.Equal(2, three.Rank);
+            Assert.Equal(2, four.Rank);
+            Assert.Equal(3, five.Rank);
+        }
+
+        [Fact]
+        public void CircularEqualPartialRanking()
+        {
+            MultiObjectiveOptimization.MultiObjectiveOptimizer multiObjectiveOptimizer = new MultiObjectiveOptimization.MultiObjectiveOptimizer();
+
+            Design one = new Design
+            {
+                AccessibilityScore = 10.0f,
+                HighestTemperatureScore = 10.0f,
+                DesiredTemperatureScore = 10.0f,
+                SpecificityScore = 10.0f,
+                StructureScore = 10.0f,
+                Job = new Job
+                {
+                    DesiredTempTolerance = 0.05f,
+                    HighestTempTolerance = 0.05f,
+                    SpecificityTolerance = 0.05f,
+                    AccessibilityTolerance = 0.05f,
+                    StructureTolerance = 0.05f
+                }
+            };
+
+            Design two = new Design
+            {
+                AccessibilityScore = 15.0f,
+                HighestTemperatureScore = 15.0f,
+                DesiredTemperatureScore = 15.0f,
+                SpecificityScore = 15.0f,
+                StructureScore = 5.0f,
+                Job = new Job
+                {
+                    DesiredTempTolerance = 0.05f,
+                    HighestTempTolerance = 0.05f,
+                    SpecificityTolerance = 0.05f,
+                    AccessibilityTolerance = 0.05f,
+                    StructureTolerance = 0.05f
+                }
+            };
+
+            Design three = new Design
+            {
+                AccessibilityScore = 5.0f,
+                HighestTemperatureScore = 12.05f,
+                DesiredTemperatureScore = 20.0f,
+                SpecificityScore = 5.0f,
+                StructureScore = 10.0f,
+                Job = new Job
+                {
+                    DesiredTempTolerance = 0.05f,
+                    HighestTempTolerance = 0.05f,
+                    SpecificityTolerance = 0.05f,
+                    AccessibilityTolerance = 0.05f,
+                    StructureTolerance = 0.05f
+                }
+            };
+
+            List<Design> designs = new List<Design>
+            {
+                one,
+                two,
+                three
+            };
+
+            multiObjectiveOptimizer.Optimize(designs, 1);
+
+            Assert.Equal(1, one.Rank);
+            Assert.Equal(1, two.Rank);
+            Assert.Equal(1, three.Rank);
         }
 
         [Fact]
