51895d9a4610ee650297fc9e2c9c9dfa9ce984a3
t-vaudry <tvaudryread@gmail.com>
2025-07-05 14:42:16 -0400

    Clean up build files that shouldn't be in repository
    Remove accidentally committed build artifacts:
    - CMake build directory and generated files
    - Catch2 downloaded sources and build files
    - Compiled object files and libraries
    - CMake cache and configuration files
    
    These files are generated during build and should not be version controlled.

--- RibosoftAlgo/build/nuget/CMakeFiles/nuget.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/nuget"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/nuget.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
