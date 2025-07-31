207dc64bbb3eaf232a030220d81a6ef7fe159b63
t-vaudry <t-vaudry@users.noreply.github.com>
2025-07-31 18:00:26 +0000

    deploy: 8eaf9f13e3f861183c9157be76c27579914fae21

--- html/search/classes_7.js
+++ html/search/classes_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['homecontroller_0',['HomeController',['../d7/d6c/classRibosoft_1_1Controllers_1_1HomeController.html',1,'Ribosoft::Controllers']]]
+  ['healthcontroller_0',['HealthController',['../d3/d15/classRibosoft_1_1Controllers_1_1HealthController.html',1,'Ribosoft::Controllers']]],
+  ['homecontroller_1',['HomeController',['../d7/d6c/classRibosoft_1_1Controllers_1_1HomeController.html',1,'Ribosoft::Controllers']]]
 ];
--- html/search/classes_c.js
+++ html/search/classes_c.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['node_0',['Node',['../d4/d01/classRibosoft_1_1CandidateGeneration_1_1Node.html',1,'Ribosoft::CandidateGeneration']]],
-  ['nucleotide_1',['Nucleotide',['../d9/dd0/classRibosoft_1_1Biology_1_1Nucleotide.html',1,'Ribosoft::Biology']]],
-  ['nucleotideattribute_2',['NucleotideAttribute',['../d9/d39/classRibosoft_1_1ValidationAttributes_1_1NucleotideAttribute.html',1,'Ribosoft::ValidationAttributes']]]
+  ['nlogconfiguration_0',['NLogConfiguration',['../d1/d3a/classRibosoft_1_1Configuration_1_1NLogConfiguration.html',1,'Ribosoft::Configuration']]],
+  ['node_1',['Node',['../d4/d01/classRibosoft_1_1CandidateGeneration_1_1Node.html',1,'Ribosoft::CandidateGeneration']]],
+  ['nucleotide_2',['Nucleotide',['../d9/dd0/classRibosoft_1_1Biology_1_1Nucleotide.html',1,'Ribosoft::Biology']]],
+  ['nucleotideattribute_3',['NucleotideAttribute',['../d9/d39/classRibosoft_1_1ValidationAttributes_1_1NucleotideAttribute.html',1,'Ribosoft::ValidationAttributes']]]
 ];
